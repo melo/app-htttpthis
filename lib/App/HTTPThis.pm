87fe377b1a37642e3f79e687a9740c480993abde
Pedro Melo <melo@simplicidade.org>
2010-07-28 08:27:12 +0100

    Build results of 307f30c (on master)

--- lib/App/HTTPThis.pm
+++ lib/App/HTTPThis.pm
@@ -1,9 +1,14 @@
 package App::HTTPThis;
+BEGIN {
+  $App::HTTPThis::VERSION = '0.001';
+}
+
+# ABSTRACT: Export the current directory over HTTP
 
 use strict;
 use warnings;
 use Plack::App::Directory;
-use Twiggy::Server;
+use Plack::Handler::Standalone;
 
 sub new {
   my $class = shift;
@@ -14,10 +19,36 @@ sub new {
 sub run {
   my ($self) = @_;
 
-  my $server = Twiggy::Server->new(port => 7007);
+  my $server = Plack::Handler::Standalone->new(port => 7007);
   print "Open your browser at http://127.0.0.1:7007/\n";
   
   $server->run(Plack::App::Directory->new->to_app);
 }
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+App::HTTPThis - Export the current directory over HTTP
+
+=head1 VERSION
+
+version 0.001
+
+=head1 AUTHOR
+
+Pedro Melo <melo@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2010 by Pedro Melo.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0
+
+=cut
+
