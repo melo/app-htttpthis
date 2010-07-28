29313e4b4e780bdd46e59557d97c25e32c1a02e9
Pedro Melo <melo@simplicidade.org>
2010-07-28 10:53:14 +0100

    Switch server from Twiggy to Plack::Handler:Standalone
    A whole lot less dependencies
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/App/HTTPThis.pm
+++ lib/App/HTTPThis.pm
@@ -3,7 +3,7 @@ package App::HTTPThis;
 use strict;
 use warnings;
 use Plack::App::Directory;
-use Twiggy::Server;
+use Plack::Handler::Standalone;
 
 sub new {
   my $class = shift;
@@ -14,7 +14,7 @@ sub new {
 sub run {
   my ($self) = @_;
 
-  my $server = Twiggy::Server->new(port => 7007);
+  my $server = Plack::Handler::Standalone->new(port => 7007);
   print "Open your browser at http://127.0.0.1:7007/\n";
 
   $server->run(Plack::App::Directory->new->to_app);
