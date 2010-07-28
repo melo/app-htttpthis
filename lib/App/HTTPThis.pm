81066da51b0e07273ece180bbb336938f7f250d5
Pedro Melo <melo@simplicidade.org>
2010-07-28 16:34:21 +0100

    Build results of 5e17012 (on master)

--- lib/App/HTTPThis.pm
+++ lib/App/HTTPThis.pm
@@ -14,9 +14,9 @@ use Pod::Usage;
 
 sub new {
   my $class = shift;
-  my $self = bless {}, $class;
+  my $self = bless {port => 7007}, $class;
 
-  GetOptions($self, "help", "man") || pod2usage(2);
+  GetOptions($self, "help", "man", "port=i") || pod2usage(2);
   pod2usage(1) if $self->{help};
   pod2usage(-verbose => 2) if $self->{man};
 
@@ -27,13 +27,14 @@ sub new {
 sub run {
   my ($self) = @_;
 
-  my $server = Plack::Handler::Standalone->new(port => 7007);
-  print "Open your browser at http://127.0.0.1:7007/\n";
+  my $server = Plack::Handler::Standalone->new(port => $self->{port});
+  print "Open your browser at http://127.0.0.1:$self->{port}/\n";
 
   $server->run(Plack::App::Directory->new->to_app);
 }
 
 1;
+
 
 __END__
 =pod
@@ -45,6 +46,25 @@ App::HTTPThis - Export the current directory over HTTP
 =head1 VERSION
 
 version 0.001
+
+=head1 SYNOPSIS
+
+    # Not to be used directly, see http_this command
+
+=head1 DESCRIPTION
+
+This class implements all the logic of the L<http_this> command.
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new App::HTTPThis object, parsing the command line arguments
+into object attribute values.
+
+=head2 run
+
+Start the HTTP server.
 
 =head1 AUTHOR
 
