581bfbadf2e6243f1fcb46457a92001debc076c4
Pedro Melo <melo@simplicidade.org>
2010-07-28 16:39:08 +0100

    Added first draft of the main class documentation
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- lib/App/HTTPThis.pm
+++ lib/App/HTTPThis.pm
@@ -9,6 +9,13 @@ use Plack::Handler::Standalone;
 use Getopt::Long;
 use Pod::Usage;
 
+=method new
+
+Creates a new App::HTTPThis object, parsing the command line arguments
+into object attribute values.
+
+=cut
+
 sub new {
   my $class = shift;
   my $self = bless {port => 7007}, $class;
@@ -20,6 +27,12 @@ sub new {
   return $self;
 }
 
+=method run
+
+Start the HTTP server.
+
+=cut
+
 sub run {
   my ($self) = @_;
 
@@ -30,3 +43,13 @@ sub run {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+    # Not to be used directly, see http_this command
+
+
+=head1 DESCRIPTION
+
+This class implements all the logic of the L<http_this> command.
+
