1434b5c25c6b668a6d257ab373264f21770b131f
Pedro Melo <melo@simplicidade.org>
2010-07-28 16:52:07 +0100

    Build results of 88032da (on master)

--- lib/App/HTTPThis.pm
+++ lib/App/HTTPThis.pm
@@ -12,25 +12,34 @@ use Plack::Handler::Standalone;
 use Getopt::Long;
 use Pod::Usage;
 
+
 sub new {
   my $class = shift;
-  my $self = bless {port => 7007}, $class;
+  my $self = bless {port => 7007, root => '.'}, $class;
 
   GetOptions($self, "help", "man", "port=i") || pod2usage(2);
   pod2usage(1) if $self->{help};
   pod2usage(-verbose => 2) if $self->{man};
 
-#  pod2usage("$0: Too many files given.\n")  if @ARGV > 1;
+  if (@ARGV > 1) {
+    pod2usage("$0: Too many roots, only single root supported");
+  }
+  elsif (@ARGV) {
+    $self->{root} = shift @ARGV;
+  }
+
   return $self;
 }
+
 
 sub run {
   my ($self) = @_;
 
   my $server = Plack::Handler::Standalone->new(port => $self->{port});
-  print "Open your browser at http://127.0.0.1:$self->{port}/\n";
+  print "Exporting '$self->{root}' available at:\n";
+  print "   http://127.0.0.1:$self->{port}/\n";
 
-  $server->run(Plack::App::Directory->new->to_app);
+  $server->run(Plack::App::Directory->new({root => $self->{root}})->to_app);
 }
 
 1;
@@ -55,6 +64,9 @@ version 0.001
 
 This class implements all the logic of the L<http_this> command.
 
+Actually, this is just a very thin wrapper around
+L<Plack::App::Directory>, that is where the magic really is.
+
 =head1 METHODS
 
 =head2 new
@@ -65,6 +77,16 @@ into object attribute values.
 =head2 run
 
 Start the HTTP server.
+
+=head1 SEE ALSO
+
+L<http_this>, L<Plack>, and L<Plack::App::Directory>.
+
+=head1 THANKS
+
+And the Oscar goes to: Tatsuhiko Miyagawa.
+
+For L<Plack>, L<Plack::App::Directory> and many many others.
 
 =head1 AUTHOR
 
